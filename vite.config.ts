4df6a5e2d7382efa405e8b09bcc229f7bd486d2a
Esmaybe <me@esmay.be>
2025-07-21 18:18:33 +0200

    yes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
     port: 3000,
+    allowedHosts: ["panel.dustydunes.net"],
   },
   plugins: [
     tsConfigPaths({
